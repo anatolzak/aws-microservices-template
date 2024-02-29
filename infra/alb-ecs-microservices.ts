4cae60291fe61ca5c069fef39ab94869d57c3f4e
anatolzak <53095479+anatolzak@users.noreply.github.com>
2024-02-29 16:48:20 +0200

    refactor

--- infra/alb-ecs-microservices.ts
+++ infra/alb-ecs-microservices.ts
@@ -5,74 +5,97 @@ import * as elbv2 from 'aws-cdk-lib/aws-elasticloadbalancingv2';
 import { Construct } from 'constructs';
 import { join } from 'path';
 
-export type Microservice = {
+type AutoScaling = {
+  minCapacity: number;
+  maxCapacity: number;
+  targetCpuUtilizationPercent?: number;
+  targetMemoryUtilizationPercent?: number;
+  requestsPerTarget?: number;
+};
+
+type Microservice = {
   folder: string;
   cpu: number;
   memoryLimitMiB: number;
   pathPattern: string;
   containerPort: number;
   priority: number;
-  autoScaling: {
-    minCapacity: number;
-    maxCapacity: number;
-    targetCpuUtilizationPercent?: number;
-    targetMemoryUtilizationPercent?: number;
-    requestsPerTarget?: number;
-  };
+  autoScaling: AutoScaling;
 };
 
 type Props = cdk.StackProps & { certificateArn: string; microservices: Microservice[] };
 
 export class EcsAlbMicroservicesStack extends cdk.Stack {
-  private vpc: cdk.aws_ec2.IVpc | undefined;
-  private albListener: cdk.aws_elasticloadbalancingv2.ApplicationListener | undefined;
-  private ecsCluster: cdk.aws_ecs.Cluster | undefined;
+  private vpc: cdk.aws_ec2.IVpc;
+  private albListener: cdk.aws_elasticloadbalancingv2.ApplicationListener;
+  private ecsCluster: cdk.aws_ecs.Cluster;
 
   constructor(scope: Construct, id: string, props: Props) {
     super(scope, id, props);
     const { certificateArn, microservices } = props;
 
-    this.setDefaultVpc();
-    this.createECSCluster();
-    this.createALB({ certificateArn });
+    this.vpc = this.getDefaultVpc();
+    this.albListener = this.createApplicationLoadBalancer(certificateArn);
+    this.ecsCluster = this.createECSCluster();
 
     microservices.forEach(microservice => {
-      this.createECSFargateService(microservice);
+      this.createMicroservice(microservice);
     });
   }
 
-  private createECSCluster() {
-    if (!this.vpc) {
-      throw new Error('VPC must be defined');
-    }
+  private getDefaultVpc(): cdk.aws_ec2.IVpc {
+    return ec2.Vpc.fromLookup(this, 'Vpc', { isDefault: true });
+  }
 
-    this.ecsCluster = new ecs.Cluster(this, 'Cluster', {
+  private createApplicationLoadBalancer(certificateArn: string): cdk.aws_elasticloadbalancingv2.ApplicationListener {
+    const alb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
+      vpc: this.vpc,
+      internetFacing: true,
+    });
+
+    // HTTP LISTENER - REDIRECT TO HTTPS
+    alb.addListener('Listener', {
+      port: 80,
+      defaultAction: elbv2.ListenerAction.redirect({
+        protocol: 'HTTPS',
+        host: '#{host}',
+        path: '/#{path}',
+        query: '#{query}',
+      }),
+    });
+
+    // HTTPS LISTENER
+    const httpsListener = alb.addListener('HTTPS Listener', {
+      port: 443,
+      certificates: [{ certificateArn }],
+      defaultAction: elbv2.ListenerAction.fixedResponse(404, {
+        contentType: 'text/plain',
+        messageBody: 'Not Found',
+      }),
+    });
+
+    new cdk.CfnOutput(this, 'ALB DNS Name', {
+      value: alb.loadBalancerDnsName,
+    });
+
+    return httpsListener;
+  }
+
+  private createECSCluster(): cdk.aws_ecs.Cluster {
+    return new ecs.Cluster(this, 'Cluster', {
       vpc: this.vpc,
     });
   }
 
-  private createECSFargateService({
-    containerPort,
-    folder,
-    pathPattern,
-    priority,
-    memoryLimitMiB,
-    cpu,
-    autoScaling: {
-      minCapacity,
-      maxCapacity,
-      targetCpuUtilizationPercent,
-      targetMemoryUtilizationPercent,
-      requestsPerTarget,
-    },
-  }: Microservice) {
-    if (!this.ecsCluster) {
-      throw new Error('ECS Cluster must be defined');
-    }
+  private createMicroservice(microservice: Microservice): void {
+    const taskDefinition = this.createTaskDefinition(microservice);
+    const fargateService = this.createFargateService(microservice, taskDefinition);
+    const targetGroup = this.addTargetGroup(microservice, fargateService);
+    this.configureAutoScaling(microservice, fargateService, targetGroup);
+  }
 
-    if (!this.albListener) {
-      throw new Error('ALB Listener must be defined');
-    }
+  private createTaskDefinition(microservice: Microservice): cdk.aws_ecs.FargateTaskDefinition {
+    const { folder, cpu, memoryLimitMiB, containerPort } = microservice;
 
     const taskDefinition = new ecs.FargateTaskDefinition(this, `${folder}TaskDef`, {
       cpu,
@@ -87,25 +110,58 @@ export class EcsAlbMicroservicesStack extends cdk.Stack {
       environment: { NODE_ENV: 'production' },
     });
 
-    const ecsService = new ecs.FargateService(this, `${folder}Service`, {
+    return taskDefinition;
+  }
+
+  private createFargateService(
+    microservice: Microservice,
+    taskDefinition: cdk.aws_ecs.FargateTaskDefinition
+  ): cdk.aws_ecs.FargateService {
+    const { folder } = microservice;
+
+    return new ecs.FargateService(this, `${folder}Service`, {
       cluster: this.ecsCluster,
       taskDefinition,
       assignPublicIp: true,
     });
+  }
 
-    const targetGroup = this.albListener.addTargets(`${folder}Target`, {
+  private addTargetGroup(
+    microservice: Microservice,
+    fargateService: cdk.aws_ecs.FargateService
+  ): cdk.aws_elasticloadbalancingv2.ApplicationTargetGroup {
+    const { folder, pathPattern, priority, containerPort } = microservice;
+
+    return this.albListener.addTargets(`${folder}Target`, {
       port: 80,
       conditions: [elbv2.ListenerCondition.pathPatterns([pathPattern])],
       priority,
       targets: [
-        ecsService.loadBalancerTarget({
+        fargateService.loadBalancerTarget({
           containerName: folder,
           containerPort,
         }),
       ],
     });
+  }
 
-    const scalableTarget = ecsService.autoScaleTaskCount({
+  private configureAutoScaling(
+    microservice: Microservice,
+    fargateService: cdk.aws_ecs.FargateService,
+    targetGroup: cdk.aws_elasticloadbalancingv2.ApplicationTargetGroup
+  ): void {
+    const {
+      folder,
+      autoScaling: {
+        minCapacity,
+        maxCapacity,
+        targetCpuUtilizationPercent,
+        targetMemoryUtilizationPercent,
+        requestsPerTarget,
+      },
+    } = microservice;
+
+    const scalableTarget = fargateService.autoScaleTaskCount({
       minCapacity,
       maxCapacity,
     });
@@ -128,45 +184,5 @@ export class EcsAlbMicroservicesStack extends cdk.Stack {
         targetGroup,
       });
     }
-  }
-
-  private createALB({ certificateArn }: { certificateArn: string }) {
-    if (!this.vpc) {
-      throw new Error('VPC must be defined');
-    }
-
-    const alb = new elbv2.ApplicationLoadBalancer(this, 'LB', {
-      vpc: this.vpc,
-      internetFacing: true,
-    });
-
-    // HTTP LISTENER - REDIRECT TO HTTPS
-    alb.addListener('Listener', {
-      port: 80,
-      defaultAction: elbv2.ListenerAction.redirect({
-        protocol: 'HTTPS',
-        host: '#{host}',
-        path: '/#{path}',
-        query: '#{query}',
-      }),
-    });
-
-    // HTTPS LISTENER
-    this.albListener = alb.addListener('HTTPS Listener', {
-      port: 443,
-      certificates: [{ certificateArn }],
-      defaultAction: elbv2.ListenerAction.fixedResponse(404, {
-        contentType: 'text/plain',
-        messageBody: 'Not Found',
-      }),
-    });
-
-    new cdk.CfnOutput(this, 'ALB DNS Name', {
-      value: alb.loadBalancerDnsName,
-    });
-  }
-
-  private setDefaultVpc() {
-    this.vpc = ec2.Vpc.fromLookup(this, 'Vpc', { isDefault: true });
   }
 }
